26687eb58b2836edd61e9f78e49d86bd53caa927
mingsound21 <mingsound@kakao.com>
2022-07-20 01:28:46 +0900

    [틀림, 다시풀기] 1715.py 맞는 답

--- greedy/1715.py
+++ greedy/1715.py
@@ -1,40 +1,23 @@
-from collections import deque
+import heapq
+from random import sample
 import sys
 input = sys.stdin.readline
 
-arr = []
 n = int(input())
+card_deck = []
 for _ in range(n):
-    arr.append(int(input()))
-arr.sort()
-arr = deque(arr)
-answer = 0
-while True:
-    newArr = deque([])
-    sum = arr.popleft() + arr.popleft()
-    answer += sum
-    newArr.append(sum)
+    heapq.heappush(card_deck, int(input()))
     
-    while(arr):
-        if len(arr) != 1:
-            if newArr[0] < arr[1]:
-                sum = arr.popleft() + newArr.popleft()
-                answer += sum
-                newArr.append(sum)
-            else:
-                sum = arr.popleft() + arr.popleft()
-                answer += sum
-                newArr.append(sum)
-        else:
-            sum = arr.popleft() + newArr.popleft()
-            answer += sum
-            newArr.append(sum)
-    if(len(newArr) == 1):
-        break
-    arr = deque(list(newArr)[:])
-    newArr = deque([])
-
+if len(card_deck) == 1: # 1개만 받는 예외 케이스 생각
+    print(0)
+else:
+    answer = 0
+    while len(card_deck) > 1 :
+        small_1 = heapq.heappop(card_deck)
+        small_2 = heapq.heappop(card_deck)
+        answer += small_1 + small_2
+        heapq.heappush(card_deck, small_1, small_2)
+        
 print(answer)
-# 최대한 작은 묶음끼리 먼저
-# sort한뒤 더하면서 작은 묶음이 반드시 앞에 있다는 보장이 사라짐
-# 20 40 41 42면 (20 + 40) + (41 + 42)
+
+# 그리디 - 현재 상황에서 가장 작은 수 2개씩 묶어 나가야 가장 효율적
