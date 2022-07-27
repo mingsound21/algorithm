10f05f925c06e3599773ff536780217bf7db9649
mingsound21 <mingsound@kakao.com>
2022-07-27 11:56:34 +0900

    틀림 | 맞는 다른 사람 풀이, 2206.py

--- graph/2206.py
+++ graph/2206.py
@@ -1,57 +1,50 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-import copy
-INF = sys.maxsize
 
-graph = []
 n, m = map(int, input().split())
+graph = []
+
+visited = [[[0] * 2 for _ in range(m)] for _ in range(n)] # n x m x 2
+visited = [0][0][0] = 1
+
 for i in range(n):
     graph.append(list(map(int, input().rstrip())))
 
-wall = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            wall.append((i, j))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-
-def bfs(a, b, tmp_graph):
-    queue = deque([(0, 0)])
-    tmp_graph[0][0] = 1
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+def bfs(x, y, z):
+    queue = deque()
+    queue.append((x, y, z))
     
     while queue:
-        x, y = queue.popleft()
-        
+        a, b, c = queue.popleft()
+        # 최종 장소에 도달하면 이동 횟수 반환
+        if a == n-1 and b == m-1:
+            return visited[a][b][c]
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m and tmp_graph[nx][ny] == 0:
-                tmp_graph[nx][ny] = tmp_graph[x][y] + 1
-                queue.append((nx, ny))
-            
-                if nx == a and ny == b:
-                    return tmp_graph[nx][ny]
-    return INF # n, m위치에 도달 못하는 경우
+        
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 다음 이동할 곳이 벽이고, 벽파괴 기회를 사용하지 않은 경우
+            if graph[nx][ny] == 1 and c == 0:
+                visited[nx][ny][1] = visited[a][b][0] + 1
+                queue.append((nx, ny, 1))
+                
+            # 다음 이동할 곳이 벽이 아니고, 아직 한번도 방문하지 않은 경우
+            elif graph[nx][ny] == 0 and visited[nx][ny][c] == 0:
+                visited[nx][ny][c] = visited[a][b][c] + 1
+                queue.append((nx, ny, c))
+    return -1
 
-day = []
-tmp_graph = copy.deepcopy(graph)
-day.append(bfs(n-1, m-1, tmp_graph)) # 벽하나도 안지웠을 때
+print(bfs(0, 0, 0))
 
-for i, j in wall:
-    tmp_graph = copy.deepcopy(graph)
-    tmp_graph[i][j] = 0
-    day.append(bfs(n-1, m-1, tmp_graph))
+# 벽 부순 여부를 3차원 행렬로 나타내기
+# visited[x][y][0] : 안부순 경로
+# visited[x][y][1] : 부순 경로
 
-day = min(day)
-if day == INF:
-    print(-1)
-else:
-    print(day)
-    
-# 1 있는 곳 모두 한번씩 지워가면서 bfs 돌리기
-# 시간 초과
+# 어렵다,, 풀이 이해도 어려워,,
