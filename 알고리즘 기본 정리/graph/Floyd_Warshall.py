e9d3257a3732d45fbf610de42d543baa1acf381c
mingsound21 <mingsound@kakao.com>
2022-08-11 16:01:13 +0900

    Floyd_Warshall.py

--- 알고리즘 기본 정리/graph/Floyd_Warshall.py
+++ 알고리즘 기본 정리/graph/Floyd_Warshall.py
@@ -1,32 +1,35 @@
 # 모든 정점 사이 최단 경로 찾는 탐색 알고리즘 : O(N^3)
 import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-INF = sys.maxsize
+n, m = map(int, input().split()) # 노드 개수, 간선 개수 입력 받기
 
-def Floyd_Warshall():
-    dist = [[INF] * n for i in range(n)] # 최단 경로를 담는 배열
+# 2차원 리스트를 만들고, 모든 값으로 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)] 
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
     
-    # 최단 경로를 담는 배열 초기화
-    for i in range(n):
-        for j in range(n):
-            dist[i][j] = a[i][j]
-            
-    for k in range(n): # 경유점
-        for i in range(n): # 시작점
-            for j in range(n): # 끝점
-                # k를 거쳤을때 경로가 더 짧아지면
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
-                    
-    return dist
-    
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b]) # k를 거쳐서 가는 경로가 더 짧으면 갱신됨
 
-n = 4 # 정점 수
-a = [[0, 2, INF, 4], [2, 0, INF, 5], [3, INF, 0, INF], [INF, 2, 1, 0]]
-
-dist = Floyd_Warshall()
-
-for i in range(n):
-    for j in range(n):
-        print(dist[i][j], end = " ")
+# 결과 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print('INFINITY', end = ' ')
+        else:
+            print(graph[a][b], end = ' ')
     print()
